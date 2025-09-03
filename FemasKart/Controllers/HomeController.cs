b223fba168afb85ae157b49cfa91d4c3717c107e
BesteBudak <besteebudak@gmail.com>
2025-09-03 10:01:52 +0300

    .

--- FemasKart/Controllers/HomeController.cs
+++ FemasKart/Controllers/HomeController.cs
@@ -23,6 +23,23 @@ namespace FemasKart.Controllers
             return View();
         }
 
+        public IActionResult LoginPage()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+                "wwwroot/sneat-1.0.0/html/auth-login-basic.html"), "text/html");
+        }
+        public IActionResult RegisterPage()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+                "wwwroot/sneat-1.0.0/html/auth-register-basic.html"), "text/html");
+        }
+
+        public IActionResult DataTablePage()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+                "wwwroot/sneat-1.0.0/html/dataTable.html"), "text/html");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
